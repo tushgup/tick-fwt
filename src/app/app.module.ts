5701959a9a2929f6c56c3402dcdc2b5cf5fc206e
Tushar Gupta <tush1998@gmail.com>
2018-04-23 01:50:32 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutes } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from 'app/services/auth-guard.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutes,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  providers: [AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCQamwKwj5uhJxCNu6BRmQcMmBW8dfxOSM',
+    authDomain: 'fwt-project-c7ce8.firebaseapp.com',
+    databaseURL: 'https://fwt-project-c7ce8.firebaseio.com',
+    projectId: 'fwt-project-c7ce8',
+    storageBucket: 'fwt-project-c7ce8.appspot.com',
+    messagingSenderId: '406700663705'
+  }
+};
